2071e65c0a8c987f4afc1951838a44c2e5743a91
aaguussilva <aaguussilva@gmail.com>
2023-09-25 13:38:13 -0300

    componente unirse a partida, y navbar

--- la_cosa/src/App.js
+++ la_cosa/src/App.js
@@ -1,16 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import CrearPartida from "./components/CrearPartida";
 import { Home } from "./components/Home";
-
+import Navbar from "./components/NavBar";
+import UnirseAPartida from "./components/UnirseAPartida";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="partida">
-        <Route path="crear" element={<CrearPartida />} />
-      </Route>
-    </Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="partida" element={<Navbar />}>
+    <Route path="crear" element={<CrearPartida />} />
+    <Route path="unir" element={<UnirseAPartida />} />
+  </Route>
+</Routes>
+
   );
 }
 
