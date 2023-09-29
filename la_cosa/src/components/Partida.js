9dbb2f643cfdb5dd62d8b3509d28be0ac899c769
francoq08 <francohugoquevedo@mi.unc.edu.ar>
2023-09-29 10:29:43 -0300

    repartir cartas

--- la_cosa/src/components/Partida.js
+++ la_cosa/src/components/Partida.js
@@ -1,20 +1,48 @@
+import {useState, useEffect} from 'react'
+import {useLocation} from 'react-router-dom'
+import axios from 'axios'
+
 export default function Partida() {
-    // Supongo que el backend me envía el listado de oponentes en un arreglo.
-    const jugadores = [
-      { nombre: "nombre1" },
-      { nombre: "nombre2" },
-      { nombre: "nombre3" },
-      { nombre: "nombre4" }
-    ];
-  
-    // Supongo que el backend me envía la mano del jugador en un arreglo.
-    const cartas = [
-      { type: "vacia" },
-      { type: "lanzallamas" },
-      { type: "vacia" },
-      { type: "lanzallamas" }
-    ];
-  
+
+const location=useLocation()
+const id= location.pathname.split('/')[2]
+const [jugadores, setJugadores] = useState([
+  { nombre: "nombre1" },
+  { nombre: "nombre2" },
+  { nombre: "nombre3" },
+  { nombre: "nombre4" }
+])
+
+const [cartas, setCartas] = useState([
+  {
+          id: 1,
+          nombre: "fedfes",
+          descripcion: "sds",
+          tipo: "panico"
+  },
+  {
+          id: 1,
+          nombre: "fedfes",
+          descripcion: "sds",
+          tipo: "panico"
+  },
+  {
+          id: 1,
+          nombre: "fedfes",
+          descripcion: "sds",
+          tipo: "panico"
+  }
+])
+
+useEffect(()=>{
+  axios.get(`http://localhost:8000/jugador?idPArtida=${id}`)
+  .then((data)=>setJugadores(data))
+  .catch((error)=>console.log(error))
+  axios.get(`http://localhost:8000/cartas/repartir?idPartida=${id}`)
+  .then((data)=>console.log(data))
+  .catch((error)=>console.log(error))
+},[])
+
     return (
       <>
         <div>
@@ -29,7 +57,7 @@ export default function Partida() {
           <h2>Mano del Jugador:</h2>
           {cartas.map((carta, index) => (
             <div key={index}>
-              <button>{carta.type}</button>
+              <button>{carta.tipo}</button>
             </div>
           ))}
         </div>
