798692899dc380f63f4af5fadabbad5999d6040f
francoq08 <francohugoquevedo@mi.unc.edu.ar>
2023-09-29 10:51:26 -0300

    agrego jugar cartas

--- la_cosa/src/App.js
+++ la_cosa/src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import CrearPartida from "./components/CrearPartida";
 import { Home } from "./components/Home";
+import Partida from "./components/Partida";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Partida />} />
       <Route path="partida">
         <Route path="crear" element={<CrearPartida />} />
       </Route>
