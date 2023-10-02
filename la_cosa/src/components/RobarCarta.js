e95dabd0e83b5f4813f17182427586d909f7f3e1
aaguussilva <aaguussilva@gmail.com>
2023-10-02 18:04:33 -0300

    visual

--- la_cosa/src/components/RobarCarta.js
+++ la_cosa/src/components/RobarCarta.js
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-function RobarCarta({idJugador, esTurno, cantidadCartasEnMano}) {
-    //los estados del jugador y las cartas en null porque no tenemos informacion 
+function RobarCarta({ idJugador, esTurno, cantidadCartasEnMano }) {
+  //los estados del jugador y las cartas en null porque no tenemos informacion
 
   const recargarPagina = () => {
     // Recarga la página actual
     window.location.reload();
   };
- 
+
   const handleDrawCard = async () => {
     try {
-        //pido al back informacion 
-      
-        // Se roba la carta y se actualiza
-      const response = await axios.put(`http://localhost:8000/jugadores/${idJugador}/robar`);
-      
+      //pido al back informacion
+
+      // Se roba la carta y se actualiza
+      const response = await axios.put(
+        `http://localhost:8000/jugadores/${idJugador}/robar`
+      );
+
       recargarPagina();
     } catch (error) {
-      console.error('Error al robar una carta:', error);
+      console.error("Error al robar una carta:", error);
     }
-  }
+  };
 
-  console.log(esTurno)
-  console.log(cantidadCartasEnMano)
-  if(!esTurno || cantidadCartasEnMano == 5){
-    return(<div></div>)
+  console.log(esTurno);
+  console.log(cantidadCartasEnMano);
+  if (!esTurno || cantidadCartasEnMano == 5) {
+    return <div></div>;
   }
 
   return (
-    <div>
-      <button onClick={handleDrawCard}>Robar Carta</button>
+    <div className="text-center mt-3">
+      <button onClick={handleDrawCard} className="btn btn-primary">
+        Robar Carta
+      </button>
     </div>
   );
 }
 
 export default RobarCarta;
-
