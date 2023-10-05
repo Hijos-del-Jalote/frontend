b349eee4031a2104ee1cb52cae767d5677d85806
aaguussilva <aaguussilva@gmail.com>
2023-10-05 15:50:15 -0300

    apiCrearPartida

--- la_cosa/src/components/NavBar.js
+++ la_cosa/src/components/NavBar.js
@@ -1,31 +1,11 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
 
 function Navbar() {
-  const [nombreJugador, setNombreJugador] = useState("");
-
   // Saco de la url mi idJugador
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const idJugador = queryParams.get("idJugador");
 
-  // Obtengo informacion del Jugador
-  useEffect(() => {
-    // Hacer una solicitud GET a http://localhost:8000/partidas cuando el componente se monte
-    if (idJugador != null) {
-      axios
-        .get(`http://localhost:8000/jugadores/${idJugador}`)
-
-        .then((response) => {
-          setNombreJugador(response.data.nombre);
-        })
-        .catch((error) => {
-          console.error(error);
-          setNombreJugador("Error el jugador no existe");
-        });
-    }
-  }, );
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-warning">
@@ -42,8 +22,6 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          {/* Agregar aquí el nombre del jugador */}
-          <span className="navbar-text">Hola {nombreJugador}</span>
         </div>
       </nav>
 
--- la_cosa/src/components/apiService.js
+++ la_cosa/src/components/apiService.js
@@ -3,26 +3,81 @@ import axios from "axios";
 const BASE_URL = "http://localhost:8000";
 
 export async function apiCrearJugador(nombreJugador) {
-    const url = BASE_URL + `/jugadores?nombre=${nombreJugador}`;
+  const url = BASE_URL + `/jugadores?nombre=${nombreJugador}`;
+
+  try {
+    const response = await axios.post(url);
+
+    if (response.status === 201) {
+      return {
+        success: true,
+        playerId: response.data.id,
+      };
+    } else {
+      return {
+        success: false,
+        message: "Error al crear el jugador",
+      };
+    }
+  } catch (err) {
+    return {
+      success: false,
+      message: "Error de red al crear el jugador",
+      error: err,
+    };
+  }
+}
+
+export async function apiObtenerJugador(idJugador) {
+  const url = BASE_URL + `/jugadores/${idJugador}`;
+
+  try {
+    const response = await axios.get(url);
+
+    if (response.status === 200) {
+      return {
+        success: true,
+        jugador: response.data,
+        nombre: response.nombre
+      };
+    } else {
+      return {
+        success: false,
+        message: "Error al obtener el jugador",
+      };
+    }
+  } catch (err) {
+    return {
+      success: false,
+      message: "Error de red al obtener el jugador",
+      error: err,
+    };
+  }
+}
+
+export async function apiCrearPartida(nombrePartida, idJugador) {
+    const url = BASE_URL + `/partidas?nombrePartida=${nombrePartida}&idHost=${idJugador}`;
   
     try {
       const response = await axios.post(url);
   
       if (response.status === 201) {
+        const partidaId = response.data.idPartida;
         return {
           success: true,
-          playerId: response.data.id,
+          partidaId: partidaId,
+          partida: response.data
         };
       } else {
         return {
           success: false,
-          message: 'Error al crear el jugador',
+          message: 'Error al crear la partida',
         };
       }
     } catch (err) {
       return {
         success: false,
-        message: 'Error de red al crear el jugador',
+        message: 'Error de red al crear la partida',
         error: err,
       };
     }
